3f9d3b2cc537020d2ed2c3d59ed217c60e50d1c2
mohamed ragab <mohamad1325@gmail.com>
2023-01-24 14:55:09 +0200

    ADSG Sim init

---
+++ adsg/src/main/java/com/sim/adsg/adsg/AdsgApplication.java
@@ -0,0 +1,13 @@
+package com.sim.adsg.adsg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdsgApplication.class, args);
+	}
+
+}
