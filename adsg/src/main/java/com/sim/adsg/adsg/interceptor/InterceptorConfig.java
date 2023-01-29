fd0cb94bd9447318dcba968944c26758ddba0e8d
Your Name <you@example.com>
2023-01-29 09:44:40 +0200

    Adding Interceptor

---
+++ adsg/src/main/java/com/sim/adsg/adsg/interceptor/InterceptorConfig.java
@@ -0,0 +1,18 @@
+package com.sim.adsg.adsg.interceptor;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@RequiredArgsConstructor
+public class InterceptorConfig extends WebMvcConfigurerAdapter {
+
+    private final LoggingInterceptor loggingInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor);
+    }
+}
---
+++ adsg/src/main/java/com/sim/adsg/adsg/interceptor/LoggingInterceptor.java
@@ -0,0 +1,32 @@
+package com.sim.adsg.adsg.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+
+@Slf4j
+@Component
+public class LoggingInterceptor extends HandlerInterceptorAdapter {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String requestUUID = UUID.randomUUID().toString();
+        request.setAttribute("requestUUID", requestUUID);
+        if (log.isInfoEnabled()) {
+//            log.info("Request with id {} : {} {}",requestUUID,request.getMethod(), request.getRequestURI());
+        }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        String requestUUID = (String) request.getAttribute("requestUUID");
+        if (log.isInfoEnabled()) {
+//            log.info("Response status for request {} is : {}",requestUUID, response.getStatus());
+        }
+    }
+}
