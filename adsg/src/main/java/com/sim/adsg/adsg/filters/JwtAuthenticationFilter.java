860b7aae3a1457a1a6bde7455b6cd14933d20397
Your Name <you@example.com>
2023-02-13 09:35:40 +0200

    Adding Jwt initial classes

--- adsg/src/main/java/com/sim/adsg/adsg/filters/JwtAuthenticationFilter.java
+++ adsg/src/main/java/com/sim/adsg/adsg/filters/JwtAuthenticationFilter.java
@@ -1,5 +1,7 @@
 package com.sim.adsg.adsg.filters;
 
+import com.sim.adsg.adsg.services.security.JwtService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -10,18 +12,37 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+
+    private final JwtService jwtService;
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         System.err.println("inside doFilterInternal");
         // :todo Get Token from Header
 
+        final String authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null ||!authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        final String jwt = authHeader.substring(7);
+
         // :todo Get username from token
+
+        String username = jwtService.extractUsername(jwt);
 
         // :todo Get all user's data from DB using username
 
         // :todo Check Token is valid
+
+        if (jwtService.isTokenValid(jwt)) {
+            System.err.println("Token is valid");
+        }
         filterChain.doFilter(request,response);
     }
 }
--- adsg/src/main/java/com/sim/adsg/adsg/services/security/JwtService.java
+++ adsg/src/main/java/com/sim/adsg/adsg/services/security/JwtService.java
@@ -1,10 +1,9 @@
 package com.sim.adsg.adsg.services.security;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +16,7 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
-  private static final String SECRET_KEY = "ADSG_SEC-KEY-NEW";
+  private static final String SECRET_KEY = "77397A24432646294A404E635266556A586E3272357538782F4125442A472D4B";
 
   public String extractUsername(String token) {
     return extractClaim(token, Claims::getSubject);
@@ -48,7 +47,7 @@ public class JwtService {
 
   public boolean isTokenValid(String token, UserDetails userDetails) {
     final String username = extractUsername(token);
-    return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    return (username.equals(userDetails.getUsername())) && validateTokenSignature(token) && !isTokenExpired(token);
   }
 
   private boolean isTokenExpired(String token) {
@@ -72,4 +71,48 @@ public class JwtService {
     byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
     return Keys.hmacShaKeyFor(keyBytes);
   }
+
+  private boolean validateTokenSignature(String token){
+    try {
+      Jws<Claims> claims = Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token);
+    } catch (Exception e) {
+      return false;
+    }
+    return true;
+  }
+
+  public boolean isTokenValid(String token) {
+    return validateTokenSignature(token) && !isTokenExpired(token);
+  }
+
+  private String generateToken(
+          Map<String, Object> extraClaims
+  ) {
+    return Jwts
+            .builder()
+            .setClaims(extraClaims)
+            .setSubject((String) extraClaims.get("username"))
+            .setIssuedAt(new Date(System.currentTimeMillis()))
+            .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+            .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+            .compact();
+  }
+  public static void main(String[] args) {
+    JwtService jwtService = new JwtService();
+//    Map<String, Object> extraClaims = new HashMap<>();
+//    extraClaims.put("username","mohamed.atef");
+//    extraClaims.put("role","ADMIN");
+//    extraClaims.put("ID",13909);
+//
+//    System.err.println(jwtService.generateToken(extraClaims));
+//
+    //String token = "eyJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiQURNSU4iLCJJRCI6MTM5MDksInVzZXJuYW1lIjoibW9oYW1lZC5hdGVmIiwic3ViIjoibW9oYW1lZC5hdGVmIiwiaWF0IjoxNjc2MjcyNDM5LCJleHAiOjE2NzYyNzM4Nzl9.kvUg0ODJ29czxFx8coz7Zqflw00pxTVwz-1OJdPsPW8";
+    String token = "eyJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiQ1JFQVRPUiIsIklEIjoxMzkwOSwidXNlcm5hbWUiOiJtb2hhbWVkLmF0ZWYiLCJzdWIiOiJtb2hhbWVkLmF0ZWYiLCJpYXQiOjE2NzYyNzI0MzksImV4cCI6MTY3NjI3Mzg3OX0.tVyWh2Mbz8Jx5-tHmHbHVxMhwFAX55xu-uTO3bFH2J0";
+    boolean tokenValid = jwtService.isTokenValid(token);
+    System.out.println("tokenValid = " + tokenValid);
+  }
+
+
+
+
 }
